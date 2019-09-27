39a1f440e54b152f6740ea7b1e3f2aad20a9bf7e
CreaViRU <navicreative@mail.ru>
2019-09-27 16:04:11 +0900

    добавления bootstrap

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+            <div class="logo"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAkEV4tvpQJSWvNbRF6n_N0Lhpa1AMJ5RfgK4dH8yMFkJxujpkIw" alt=""></div>
+            <nav>
+                <ul>
+                    <li>каталог знаний</li>
+                    <li>о проекте</li>
+                    <li>наставникам</li>
+                </ul>
+            </nav>
+            <div class="user">
+                <span class="user-name">nelav veoray</span>
+                <img class="user-img" src="./1557216671.5403_tunyra_n.jpg" alt="">
+        </div>
+    </div>
+  <!--  <div class="hero img">
+<div class="text">
+    <h1>Каждый каждому учитель</h1>
+    <p>Найдите наставника и получите необходимые навыки
+        или станьте наставником и зарабатывайте на своей экспертности</p>
+        <a href="buttuon-right">с чего начать</a>
+</div>
+    </div>-->
+</body>
+</html>
