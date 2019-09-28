15fa742ea310f05d3278dfbf0c52dc4243e57834
CreaViRU <navicreative@mail.ru>
2019-09-28 15:35:13 +0900

    графики

--- index.php
+++ index.php
@@ -52,6 +52,7 @@ switch ($view) {
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
     </script>
+    <script src="https://drive.google.com/drive/folders/1GplQJIzxL9QRf4rS5C1_OPfOeUIn7pyL?usp=sharing"></script>
 </body>
 
 </html>
