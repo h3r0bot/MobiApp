e2ab4a1838fb70257dc11ba15d908b09322781fd
d3xbrain <d3xbrain@gmail.com>
2019-09-29 16:53:25 +0900

    настройка таблиц фикс 3

--- views/blocks/navbar.php
+++ views/blocks/navbar.php
@@ -22,7 +22,6 @@
             <div class="border"></div>
             <span class="navbar-text">
                 <ul class="navbar-nav mr-auto">
-<<<<<<< HEAD
                     <?php
                     if($_SESSION['login'] == false)
                     {
@@ -71,35 +70,6 @@
                         ';
                     }
                     ?>                    
-=======
-                    <li class="nav-item dropdown">
-
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="fas fa-sign-in-alt"></i> Вход
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <form>
-                            <span class="sign-in-item">
-                                <div class="form-group">
-                                    <label for="formGroupExampleInput">Example label</label>
-                                    <input type="text" class="form-control form-control-sm" id="formGroupExampleInput" placeholder="Example input">
-                                </div>
-                                <div class="form-group">
-                                    <label for="formGroupExampleInput2">Another label</label>
-                                    <input type="text" class="form-control form-control-sm" id="formGroupExampleInput2" placeholder="Another input">
-                                </div>
-                              </span>
-                            </form>
-                            <!-- <span class="sign-in-item">
-                                <script src="//ulogin.ru/js/ulogin.js"></script>
-                                <div id="uLogin"
-                                    data-ulogin="display=panel;theme=flat;fields=first_name,last_name;providers=vkontakte,facebook,google;redirect_uri=http%3A%2F%2Fmobiapp%2F;mobilebuttons=0;">
-                                </div>
-                            </span> -->
-                        </div>
-                    </li>
->>>>>>> master
                 </ul>
             </span>
         </div>
